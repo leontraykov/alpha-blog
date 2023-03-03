8993d0f0eab4ff2371c182741e7ba992d19362d6
leont <leon.traykov@gmail.com>
2023-03-03 16:42:32 -0400

    Assosiation added for users-articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :username, :email, presence: true, uniqueness: { case_sensitive: false }
-  
-  validates :username, length: {minimum: 3, maximum: 25}
+  has_many :articles
 
-  validates :email, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :username, :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :username, length: {minimum: 3, maximum: 25}
+  validates :email, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
