15ee0bb9b94bb831abcebb6a2022429ac9ce85a1
leont <leon.traykov@gmail.com>
2023-03-04 16:33:03 -0400

    User show page added with gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    email = user.email
+    hash = Digest::MD5.hexdigest(email)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+
+    image_tag(gravatar_url, alt:user.username, class:"rounded-circle shadow-lg mx-auto d-block")
+  end
 end
