3c5060ab19ff2702bfbdaa73150008c7bd6ea386
leont <leon.traykov@gmail.com>
2023-03-04 13:43:26 -0400

    user authorization and sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  before_save { self.username = username.capitalize }
   has_many :articles
 
   validates :username, :email, presence: true, uniqueness: { case_sensitive: false }
   validates :username, length: {minimum: 3, maximum: 25}
   validates :email, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :articles
   root 'pages#home'
-  get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
+
+  resources :articles
+  resources :users, except: [:new]
+
+  get 'signup', to: 'users#new'
+
+  post 'users', to: 'users#create'
+
 end
