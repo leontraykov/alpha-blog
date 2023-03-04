33afe91484950842284bdc2578ea4b89efa7fa4c
leont <leon.traykov@gmail.com>
2023-03-04 14:09:00 -0400

    edit user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params) 
+      flash[:notice] = "Your information successfully updated!"
+      redirect_to user_path
+    else
+      render "edit"
+    end
+  end
+
   private
 
   def user_params
